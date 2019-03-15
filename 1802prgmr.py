d145b5c979db873ef138c781335c638e37eb623f
Eric <erichmatt@gmail.com>
2019-03-14 22:30:07 -0400

    Its a start

--- 1802prgmr.py
+++ 1802prgmr.py
@@ -1,8 +1,12 @@
-import pyb
-from pyb import *
-import os
-
+#import pybk
+#from pyb import *
+#import os
+import struct
 program = None
 
 with open("out.bin", "rb") as f:
-    program = f
+    program = f.read()
+if(((program[1] & 0b10000000)) != 0):
+	print ("8th bit is 1")
+else:
+	print ("8th bit is 0")

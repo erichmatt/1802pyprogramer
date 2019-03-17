3dcb56b5690d1690535a6c01818380c3d08f0643
Cyrus <cyrusb252@gmail.com>
2019-03-17 10:06:39 -0400

    I did a thing
    I did a small thing

--- 1802prgmr.py
+++ 1802prgmr.py
@@ -10,3 +10,10 @@ if(((program[1] & 0b10000000)) != 0):
 	print ("8th bit is 1")
 else:
 	print ("8th bit is 0")
+	
+#not totally sure how reading binary files works in python but this was my original idea
+for i in program:
+	if i & 0b10000000 == 1:
+		print(f"bit {i} is 1")
+	else:
+		print(f"bit {i} is 0")
